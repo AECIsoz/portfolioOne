57ded9fcbed8992b9988ed627062ae9db3b9e436
AECIsoz <anna.isoz@outlook.com>
2021-08-20 07:19:50 +0200

    Add presentation content, start js coding

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('slide-one-title').hidden = true;
+
+let startPresentation = function() {
+    document.getElementById('start').hidden = true;
+}
+
+document.getElementById('initiate').onclick = startPresentation;
