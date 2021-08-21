fd104860595b18f3d83491c1e9e537902d5875a3
AECIsoz <anna.isoz@outlook.com>
2021-08-21 10:15:05 +0200

    Rework layout presentation slides

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 let start = document.getElementById('start');
 let initiate = document.getElementById('initiate');
-let slideOne = document.getElementById('slide-one');
+let presentationSlides = document.getElementById('presentation-slides');
+/*let slideOne = document.getElementById('slide-one')
 let slideOneTitle = document.getElementById('slide-one-title');
+let next = document.getElementById('next');*/
 
-slideOneTitle.style.display = 'none';
+presentationSlides.style.display = 'none';
+/*slideOneTitle.style.display = 'none';
 slideOne.style.display = 'none';
+next.style.display = 'none';*/
 
 function startPresentation() {
     start.style.display = 'none';
-    slideOneTitle.style.display = 'flex';
+    presentationSlides.style.display = 'flex';
+    /*slideOneTitle.style.display = 'flex';
     slideOne.style.display = 'flex';
+    slideOne.style.flexDirection = 'row';
+    next.style.display = 'flex';*/
 }
 
 initiate.addEventListener('click', startPresentation);
